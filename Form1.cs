3db6573503e3db2498672a33369f568e34e3ec4e
LennartMombaur <1058072@bkrnet.de>
2022-12-01 11:07:16 +0100

    funktionen hinzugefügt

--- Form1.cs
+++ Form1.cs
@@ -16,24 +16,48 @@ namespace Lottoprogramm
         {
             InitializeComponent();
         }
-        int[] lottozahlen= new int[6];
-        private void auslosen_Click(object sender, EventArgs e)
-        {
 
+        int[] lottozahlen= new int[6];
+        Random zahlengen = new Random();
+        int zahlenbereich = 49;
+        bool losen=false;
+        public void auslosen_Click(object sender, EventArgs e)
+        {
+            losen = true;
+            if (losen == true)
+            {
+                for (int i = 0; i < lottozahlen.Length; i++)
+                {
+                   lottozahlen[i] = zahlengen.Next();
+
+                }
+            }
+            else
+            {
+                for (int i = 1; i < lottozahlen.Length; i++)
+                {
+                    lottozahlen[i] = i;
+                }
+            }
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            zahl1.Text = Convert.ToString(lottozahlen[0]);
+            zahl2.Text = Convert.ToString(lottozahlen[1]);
+            zahl3.Text = Convert.ToString(lottozahlen[2]);
+            zahl4.Text = Convert.ToString(lottozahlen[3]);
+            zahl5.Text = Convert.ToString(lottozahlen[4]);
+            zahl6.Text = Convert.ToString(lottozahlen[5]);
         }
 
         private void zahl1_TextChanged(object sender, EventArgs e)
         {
-
+            
         }
 
         private void zahl2_TextChanged(object sender, EventArgs e)
         {
-
+           
         }
 
         private void zahl3_TextChanged(object sender, EventArgs e)
@@ -43,17 +67,17 @@ namespace Lottoprogramm
 
         private void zahl4_TextChanged(object sender, EventArgs e)
         {
-
+           
         }
 
         private void zahl5_TextChanged(object sender, EventArgs e)
         {
-
+            
         }
 
         private void zahl6_TextChanged(object sender, EventArgs e)
         {
-
+          
         }
     }
 }
